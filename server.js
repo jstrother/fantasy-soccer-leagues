44f46678cfcea9aa5bc99125213ab95e60caf6f4
Jim Strother <jim.strother@gmail.com>
2017-04-09 18:19:50 -0700

    starting scraping code in server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 // server.js
+
+const path = require('path'),
+			express = require('express'),
+			app = express(),
+			server = require('http').Server(app),
+			request = require('request'),
+			cheerio = require('cheerio'),
+			PORT = 8080,
+			scheduleURL = 'http://www.mlssoccer.com/schedule?month=all&year=2017&club=select&club_options=9&op=Update&form_build_id=form-ORn_kjWBAHvfd2ahH5gk9xi5HZpp0OTYpCYHbemGCFs&form_id=mp7_schedule_hub_search_filters_form';
+
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(`${__dirname}/public/index.html`));
+});
+
+function scheduleGrabber(url) {
+	request(url, (error, response, body) => {
+		if (!error) {
+			// trying to catch the full schedule
+			let $ = cheerio.load(body),
+					schedule = $();
+		} else {
+			console.log(`The following error was encountered: ${error}`)
+		}
+	})
+}
