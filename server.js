dcffda0984ae462b44af937190949b02b0c37b85
Jim Strother <jim.strother@gmail.com>
2017-04-13 22:28:57 -0700

    end of night commit: made some more notes as to what exactly i need to make this work

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const path = require('path'),
 			request = require('request'),
 			cheerio = require('cheerio'),
 			PORT = 8080,
-			scheduleURL = 'http://www.mlssoccer.com/schedule?month=all&year=2017&club=select&club_options=9&op=Update&form_build_id=form-ORn_kjWBAHvfd2ahH5gk9xi5HZpp0OTYpCYHbemGCFs&form_id=mp7_schedule_hub_search_filters_form';
+			scheduleURL = 'http://www.mlssoccer.com/schedule?month=all&year=2017&club=select&club_options=9&op=Update&form_build_id=form-ORn_kjWBAHvfd2ahH5gk9xi5HZpp0OTYpCYHbemGCFs&form_id=mp7_schedule_hub_search_filters_form',
+			standingsURL = 'http://www.mlssoccer.com/standings'
+			playersURL = 'http://www.mlssoccer.com/players?page=',
+			playerStatsURL = 'http://www.mlssoccer.com/players/';
+// playersURL ends as it does so that a for loop from 0-20 can be used via concatenation
+// playerStatsURL will have the player's name concatenated to the end of it
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
