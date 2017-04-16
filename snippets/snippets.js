7a573013ce2b5d33287880c0e0e3cb1416c4c6f3
Jim Strother <jim.strother@gmail.com>
2017-04-16 10:30:03 -0700

    forgot to push snippets.js to github

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -9,39 +9,58 @@ mongoose.connection.on('error', err => {
 
 mongoose.connection.once('open', () => {
 	const scheduleSchema = mongoose.Schema({
-			masterMatchList: {type: Array, uniquie: true},
-			numSeasonMatches: {type: Number} // simple .length
-		});
-
-	const playerSchema = mongoose.Schema ({
-		playerUniqueID: {type: String, unique: true},
-		playerName: {type: String, unique: true},
-		playerClub: {type: String} // reference to info scrapped from MLS
-		playerPosition: {type: String},
-		playerStatistics: {type: Array},
-		matchDayList: {type: Function}  // based off playerClub.matchDayList
-	});
-
-	const matchSchema = mongoose.Schema({
-		matchUniqueID: {type: String, unique: true},
-		homeClub: {type: String, unique: true},
-		awayClub: {type: String, unique: true},
-		homeScore: {type: Number},
-		awayScore: {type: Number}
-	});
-
-	const clubSchema = mongoose.Schema({
-		clubName: {type: String, unique: true}, //
-		clubRoster: {type: Array, unique: true}, // sorted out from player.playerClub
-		matchDayList: {type: Function, unique: true}  // sorted out from schedule.masterMatchList
-	});
-
-	const userSchema = mongoose.Schema({
-		userName: {type: String, unique: true},
-		userPassword: {type: String},
-		fantasyClub: {type: String, unique: true},
-		fantasyLeague: {type: String},
-		fantasyDivision: {type: String},
-		fantasyChampsLeague: {type: Boolean}
-	});
+					masterMatchList: {type: Array, uniquie: true},
+					numSeasonMatches: Number // simple .length
+				}),
+				playerSchema = mongoose.Schema ({
+					playerUniqueID: {type: String, unique: true},
+					playerName: {type: String, unique: true},
+					playerClub: String,
+					playerPosition: String,
+					playerStatistics: Array,
+					playerValue: Number, // in millions of $$$'s
+					matchDayList: Function  // based off playerClub.matchDayList
+				}),
+				matchSchema = mongoose.Schema({
+					matchUniqueID: {type: String, unique: true},
+					homeClub: {type: String, unique: true},
+					awayClub: {type: String, unique: true},
+					homeScore: Number,
+					awayScore: Number,
+					matchDate: Date,
+					totalPayroll: Number
+				}),
+				clubSchema = mongoose.Schema({
+					clubName: {type: String, unique: true}, //
+					clubRoster: {type: Array, unique: true}, // sorted out from player.playerClub
+					matchDayList: {type: Function, unique: true}  // sorted out from schedule.masterMatchList
+				}),
+				userSchema = mongoose.Schema({
+					userName: {type: String, unique: true},
+					userPassword: String,
+					fantasyClub: {type: String, unique: true},
+					fantasyLeague: String,
+					fantasyDivision: String,
+					fantasyChampsLeague: Boolean
+				}),
+				fantasyMatch = mongoose.Schema({
+					matchUniqueID: {type: String, unique: true}
+					homeClub: {type: String, unique: true},
+					awayClub: {type: String, unique: true},
+					homeScore: Number,
+					awayScore: Number
+				}),
+				fantasyRegSeasonSchedule = mongoose.Schema({
+					masterMatchList: {type: Array, unique: true}, // listed by fantasyMatch.matchUniqueID
+					numRegSeasonMatches: Number
+				}),
+				fantasyLeagueCupSchedule = mongoose.Schema({
+					masterMatchList: {type: Array, unique: true}, // listed by fantasyMatch.matchUniqueID
+					roundTracker: Function
+				}),
+				fantasyChampsLeagueSchedule = mongoose.Schema({
+					masterMatchList: {type: Array, unique: true}, // listed by fantasyMatch.matchUniqueID
+					roundTracker: Function,
+					groupStage: Boolean
+				});
 });
