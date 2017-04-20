07ee7b94e85114936ed4b1feca3f4b2925da3af1
Jim Strother <jim.strother@gmail.com>
2017-04-20 09:00:00 -0700

    having issues getting CRUD to work with player

--- db-test.js
+++ db-test.js
@@ -7,41 +7,58 @@ mongoose.connection.on('error', err => {
     console.error('Could not connect.  Error:', err);
 });
 
+let player = {
+	playerUniqueID: 1,
+	playerName: 'Jim Strother',
+	playerURL: `http://www.mlssoccer.com/players/jim-strother`,
+	playerClub: 'Seattle Sounders FC',
+	playerPosition: 'Defender',
+	playerStats: {
+		gamesPlayed: 0,
+		gamesStarted: 0,
+		minutesPlayed: 0,
+		goals: 0,
+		assists: 0,
+		shotsTaken: 0,
+		shotsOnGoal: 0,
+		foulsCommitted: 0,
+		timesOffside: 0,
+		yellowCards: 0,
+		redCards: 0
+	},
+	playerValue: 50, // in millions of $$$'s
+	playerSchedule: [
+		'@ Portland',
+		'Vancouver'
+	]
+};
+
 mongoose.connection.once('open', () => {
 	console.log('player', player);
-  const create = (playerUniqueID, playerName, playerURL, playerClub, playerPosition, playerStats, playerValue, playerSchedule) => {
-  	let player = {
-			playerUniqueID,
-			playerName,
-			playerURL,
-			playerClub,
-			playerPosition,
-			playerStats: {
-				gamesPlayed,
-				gamesStarted,
-				minutesPlayed,
-				goals,
-				assists,
-				shotsTaken,
-				shotsOnGoal,
-				foulsCommitted,
-				timesOffside,
-				yellowCards,
-				redCards
-			},
-			playerValue, // in millions of $$$'s
-			playerSchedule
-		};
+  const create = player => {
   	Player.create(player, (err, player) => {
       if (err || !player) {
-        console.error("Could not create player ", playerName);
+        console.error("Could not create player", player);
+        console.log('Error:', err);
         mongoose.disconnect();
         return;
       }
-      console.log("Created player ", player.playerName);
+      console.log("Created player", player);
       mongoose.disconnect();
    });
   };
+  const del = player => {
+    Player.findOneAndRemove({player}, (err, player) => {
+        if (err || !player) {
+            console.error("Could not delete player", player);
+            mongoose.disconnect();
+            return;
+        }
+        console.log("Deleted player", player);
+        mongoose.disconnect();
+    });
+  };
 
-	create(1, 'Jim Strother', `http://www.mlssoccer.com/players/${this.playerName}`, 'Seattle Sounders FC', 'Defender', {0,0,0,0,0,0,0,0,0,0,0}, 50, ['@ Portland', 'Vancouver']);
+	// create(player);
+	del(player);
 });
