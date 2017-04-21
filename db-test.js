9e429892ad9801030161e187b7498b525b689fc9
Jim Strother <jim.strother@gmail.com>
2017-04-21 12:17:12 -0700

    getting Player CRUD to work

--- db-test.js
+++ db-test.js
@@ -1,31 +1,28 @@
 const mongoose = require('mongoose'),
 			Player = require('./models/player_model.js');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/');
+mongoose.connect('mongodb://localhost/players');
 
 mongoose.connection.on('error', err => {
     console.error('Could not connect.  Error:', err);
 });
 
 let player = {
-	playerUniqueID: 1,
 	playerName: 'Jim Strother',
 	playerURL: `http://www.mlssoccer.com/players/jim-strother`,
 	playerClub: 'Seattle Sounders FC',
-	playerPosition: 'Defender',
-	playerStats: {
-		gamesPlayed: 0,
-		gamesStarted: 0,
-		minutesPlayed: 0,
-		goals: 0,
-		assists: 0,
-		shotsTaken: 0,
-		shotsOnGoal: 0,
-		foulsCommitted: 0,
-		timesOffside: 0,
-		yellowCards: 0,
-		redCards: 0
-	},
+	playerPosition: 'Midfielder',
+	gamesPlayed: 0,
+	gamesStarted: 0,
+	minutesPlayed: 0,
+	goals: 0,
+	assists: 0,
+	shotsTaken: 0,
+	shotsOnGoal: 0,
+	foulsCommitted: 0,
+	timesOffside: 0,
+	yellowCards: 0,
+	redCards: 0,
 	playerValue: 50, // in millions of $$$'s
 	playerSchedule: [
 		'@ Portland',
@@ -47,10 +44,35 @@ mongoose.connection.once('open', () => {
       mongoose.disconnect();
    });
   };
+  const read = player => {
+    Player.findOne(player, (err, player) => {
+        if (err || !player) {
+            console.error("Could not read player", player);
+    console.log('Error:', err);
+            mongoose.disconnect();
+            return;
+        }
+        console.log("Read player", player);
+        mongoose.disconnect();
+    });
+	};
+	const update = player => {
+	    Player.findOneAndUpdate({playerName: 'Jim Strother'} ,player, (err, player) => {
+	        if (err || !player) {
+	            console.error("Could not update player", player);
+	    console.log('Error:', err);
+	            mongoose.disconnect();
+	            return;
+	        }
+	        console.log("Updated player", player);
+	        mongoose.disconnect();
+	    });
+	};
   const del = player => {
-    Player.findOneAndRemove({player}, (err, player) => {
+    Player.findOneAndRemove(player, (err, player) => {
         if (err || !player) {
             console.error("Could not delete player", player);
+        console.log('Error:', err);
             mongoose.disconnect();
             return;
         }
@@ -60,5 +82,7 @@ mongoose.connection.once('open', () => {
   };
 
 	// create(player);
-	del(player);
+	// read({});
+	update(player);
+	// del(player);
 });
--- models/player_model.js
+++ models/player_model.js
@@ -1,27 +1,40 @@
 const mongoose = require('mongoose'),
 
 			playerSchema = mongoose.Schema ({
-				playerUniqueID: {type: Number, unique: true},
-				playerName: {type: String, unique: true},
+				playerName: String,
 				playerURL: {type: String, unique: true},
 				playerClub: String,
 				playerPosition: String,
-				playerStats: {
-					gamesPlayed: Number,
-					gamesStarted: Number,
-					minutesPlayed: Number,
-					goals: Number,
-					assists: Number,
-					shotsTaken: Number,
-					shotsOnGoal: Number,
-					foulsCommitted: Number,
-					timesOffside: Number,
-					yellowCards: Number,
-					redCards: Number
-				},
+				gamesPlayed: Number,
+				gamesStarted: Number,
+				minutesPlayed: Number,
+				goals: Number,
+				assists: Number,
+				shotsTaken: Number,
+				shotsOnGoal: Number,
+				foulsCommitted: Number,
+				timesOffside: Number,
+				yellowCards: Number,
+				redCards: Number,
 				playerValue: Number, // in millions of $$$'s
 				playerSchedule: Array  // based off playerClub.clubSchedule
 			}),
+
+			// playerSchema.virtual('playerStats').get(() => {
+			// 	return [
+			// 		this.gamesPlayed,
+			// 		this.gamesStarted,
+			// 		this.minutesPlayed,
+			// 		this.goals,
+			// 		this.assists,
+			// 		this.shotsTaken,
+			// 		this.shotsOnGoal,
+			// 		this.foulsCommitted,
+			// 		this.timesOffside,
+			// 		this.yellowCards,
+			// 		this.redCards
+			// 	];
+			// }),
 
 			Player = mongoose.model('Player', playerSchema);
 
