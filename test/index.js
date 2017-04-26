bfec2a5598843e9acdc3022fe2fc08e6713b7733
Jim Strother <jim.strother@gmail.com>
2017-04-25 21:40:37 -0700

    trying to get tests working, failing on before hook

--- test/index.js
+++ test/index.js
@@ -1,25 +1,41 @@
 const mongoose = require('mongoose'),
-			chai = require('chai'),
-			chaiHTTP = require('chai-http'),
-			should = chai.should(),
-			Club = require('../models/club_model.js'),
-			sampleClub = require('../sample-club.js'),
-			Player = require('../models/player_model.js'),
-			samplePlayer = require('../sample-player.js'),
-			read = require('../snippets/snippets.js').read;
+	chai = require('chai'),
+	chaiHTTP = require('chai-http'),
+	should = chai.should(),
+	Club = require('../models/club_model.js'),
+	sampleClub = require('../sample-club.js'),
+	Player = require('../models/player_model.js'),
+	samplePlayer = require('../sample-player.js'),
+	{read} = require('../snippets/snippets.js');
 
 mongoose.Promise = global.Promise;
-
+console.log('Hello!');
 describe('Club Roster', () => {
-	it('should add player from ref', (done) => {
-		mongoose.connect('mongodb://localhost/fantasy-league-test');
-		mongoose.connection.once('open', () => {
-			mongoose.dropDatabase()
-				.then((first) => {
-					console.log('first', first);
-					mongoose.disconnect();
-					done();
-				});
+	before(() => {
+		return new Promise((resolve, reject) => {
+			mongoose.connect('mongodb://localhost/fantasy-league', err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+	beforeEach(() => {
+		return new Promise((resolve, reject) => {
+			mongoose.connection.dropDatabase()
+			.then(result => resolve(result))
+			.catch(err => reject(err))
+		});
+	});
+	afterEach();
+	after(done => {
+		mongoose.disconnect();
+		done();
+	});
+	describe('Player', () => {
+		it('should add player from ref', (done) => {
+
 		});
 	});
 });
