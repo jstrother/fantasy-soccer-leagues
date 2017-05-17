123bffe62e6eb44ddfd12e372b90263f135393e9
Jim Strother <strotherwebdev@gmail.com>
2017-05-17 06:30:09 +0000

    updated reqs-and-ideas file with ideas for api interactions; added such files to api_functions; 4 new working tests

--- api_functions.js
+++ api_functions.js
@@ -13,14 +13,69 @@ function scheduleGrabber(roundId) {
   };
   
   return rp(options)
+  .then(() => {
+    
+  })
   .catch(error => {
     console.log(`error: ${error}`);
   });
 }
 
-function playerGrabber() {
+function playerGameStatsGrabber(date, playerId) {
+  let options = {
+    uri: `https://api.fantasydata.net/soccer/v2/json/PlayerGameStatsByPlayer/${date}/${playerId}`,
+    headers: {
+      'Ocp-Apim-Subscription-Key': key
+    },
+    json: true
+  };
   
+  return rp(options)
+  .then(() => {
+    
+  })
+  .catch(error => {
+    console.log(`error: ${error}`);
+  });
+}
+
+function playerSeasonStatsGrabber(roundId, playerId) {
+  let options = {
+    uri: `https://api.fantasydata.net/soccer/v2/{format}/PlayerSeasonStatsByPlayer/${roundId}/${playerId}`,
+    headers: {
+      'Ocp-Apim-Subscription-Key': key
+    },
+    json: true
+  };
+  
+  return rp(options)
+  .then(() => {
+    
+  })
+  .catch(error => {
+    console.log(`error: ${error}`);
+  });
+}
+
+function competitionFixturesGrabber(competitionId) {
+  let options = {
+    uri: `https://api.fantasydata.net/soccer/v2/json/CompetitionDetails/${competitionId}`,
+    headers: {
+      'Ocp-Apim-Subscription-Key': key
+    },
+    json: true
+  };
+  
+  return rp(options)
+  .then(() => {
+    
+  })
+  .catch(error => {
+    console.log(`error: ${error}`);
+  });
 }
 
 exports.scheduleGrabber = scheduleGrabber;
-exports.playerGrabber = playerGrabber;
+exports.playerGameStatsGrabber = playerGameStatsGrabber;
+exports.playerSeasonStatsGrabber = playerSeasonStatsGrabber;
+exports.competitionFixturesGrabber = competitionFixturesGrabber;
--- test/api-test.js
+++ test/api-test.js
@@ -2,7 +2,7 @@ const
     // import common modules
     { mongoose, apiTestConnection } = require('./common.js'),
     // import api functions
-    { scheduleGrabber } = require('../api_functions.js');
+    { scheduleGrabber, playerGameStatsGrabber, playerSeasonStatsGrabber, competitionFixturesGrabber } = require('../api_functions.js');
 
 before(done => {
 	mongoose.connect(apiTestConnection);
@@ -22,15 +22,61 @@ after(done => {
 	done();
 });
 	
-describe('MLS Schedule', () => {
-    it('should retrieve current MLS schedule', () => {
-	    const roundId = 117;
-	    return scheduleGrabber(roundId)
-	    .then((schedule) => {
-		  	schedule.should.be.an.object;
-			})
-			.catch(error => {
-				console.log(`error: ${error}`);
-			});
-    });
+describe('Schedule Grabber', () => {
+  it('should retrieve current MLS schedule', () => {
+  	const roundId = 117;
+  	
+  	return scheduleGrabber(roundId)
+  	.then((schedule) => {
+        schedule.should.be.an.object;
+    })
+		.catch(error => {
+			console.log(`error: ${error}`);
+		});
+  });
+});
+
+describe('Player Game Stats Grabber', () => {
+	it('should retrieve a players stats by game', () => {
+		const date = '2017-05-06',
+			playerId = 90032467;
+		
+		return playerGameStatsGrabber(date, playerId)
+		.then((player) => {
+			player.should.be.an.object;
+		})
+		.catch(error => {
+			console.log(`error: ${error}`);
+		});
+	});
+});
+
+describe('Player Season Stats Grabber', () => {
+	it('should retrieve a players stats of the season so far', () => {
+		const roundId = 117,
+			playerId = 90032467;
+		
+		return playerSeasonStatsGrabber(roundId, playerId)
+		.then((player) => {
+			player.should.be.an.object;
+		})
+		.catch(error => {
+			console.log(`error: ${error}`);
+		});
+	});
+});
+
+describe('Competition Grabber', () => {
+	it('should retrieve the roundId of regular season and list of players in league', () => {
+		const competitionId = 8;
+		
+		return competitionFixturesGrabber(competitionId)
+		.then((player) => {
+			player.should.be.an.object;
+		})
+		.catch(error => {
+			console.log(`error: ${error}`);
+		});
+	})
+	.timeout(5000);
 });
