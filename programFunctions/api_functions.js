129375198f84cff3b2c7c589f901c655f526570d
Jim Strother <strotherwebdev@gmail.com>
2017-06-02 23:10:28 +0000

    working on api v2.0 functions

--- programFunctions/api_functions.js
+++ programFunctions/api_functions.js
@@ -1,137 +1,26 @@
 const rp = require('request-promise'),
     key = require('../config.js').API_KEY,
-    baseURL = 'https://api.soccerama.pro/v1.2',
+    baseURL = 'https://soccer.sportmonks.com/api/v2.0',
     toInclude = '&include=';
 
-function competitionGrabber(competitionId) {
-  const endpoint = `${baseURL}/competitions/`,
-    included = `${toInclude}currentSeason`,
+function leagueGrabber(leagueId) {
+  const endpoint = `${baseURL}/leagues/`,
+    included = `${toInclude}seasons,season`,
     competition = {
-      uri: `${endpoint}${competitionId}${key}${included}`,
+      uri: `${endpoint}${leagueId}${key}${included}`,
       json: true
-  };
+    };
   
   return rp(competition)
   .then(competition => {
+    console.log(competition);
     return competition;
   })
   .catch(error => {
-    console.log(`competitionGrabber error: ${error}`);
+    console.log(`leagueGrabber error: ${error}`);
   });
 }
 
-function seasonGrabber(seasonId) {
-  const endpoint = `${baseURL}/seasons/`,
-    included = `${toInclude}matches`,
-    season = {
-      uri: `${endpoint}${seasonId}${key}${included}`,
-      json: true
-    };
-  
-  return rp(season)
-  .then(season => {
-    return season;
-  })
-  .catch(error => {
-    console.log(`seasonGrabber error: ${error}`);
-  });
-}
+leagueGrabber(501);
 
-function teamsGrabber(seasonId) {
-  const endpoint = `${baseURL}/teams/season/`,
-    included = `${toInclude}players`,
-    teams = {
-      uri: `${endpoint}${seasonId}${key}${included}`,
-      json: true
-    };
-  
-  return rp(teams)
-  .then(teams => {
-    console.log(teams);
-    return teams;
-  })
-  .catch(error => {
-    console.log(`teamsGrabber error: ${error}`);
-  });
-}
-
-// teamsGrabber(741);
-
-function playersGrabber(playerId) {
-  const endpoint = `${baseURL}/players/`,
-    included = `${toInclude}team,events,lineups,position`,
-    player = {
-        uri: `${endpoint}${playerId}${key}${included}`,
-        json: true
-    };
-  
-  return rp(player)
-  .then(player => {
-    return player;
-  })
-  .catch(error => {
-    console.log(`playersGrabber error: ${error}`);
-  });
-}
-
-function playersByTeamGrabber(teamId) {
-  const endpoint = `${baseURL}/players/team/`,
-    included = `${toInclude}team,events,season,lineups,position`,
-    player = {
-        uri: `${endpoint}${teamId}${key}${included}`,
-        json: true
-    };
-  
-  return rp(player)
-  .then(player => {
-    console.log(player);
-    return player;
-  })
-  .catch(error => {
-    console.log(`playersGrabber error: ${error}`);
-  });
-}
-
-playersByTeamGrabber(690);
-
-function matchGrabber(matchId) {
-  const endpoint = `${baseURL}/matches/`,
-    included = `${toInclude}competition,season,homeTeam,awayTeam,events,lineup`,
-    match = {
-      uri: `${endpoint}${matchId}${key}${included}`,
-      json: true
-    };
-  
-  return rp(match)
-  .then(match => {
-    return match;
-  })
-  .catch(error => {
-    console.log(`matchGrabber error: ${error}`);
-  });
-}
-
-function matchStatsGrabber(matchId) {
-  const endpoint = `${baseURL}/statistics/match/`,
-    included = `${toInclude}team`,
-    matchStats = {
-      uri: `${endpoint}${matchId}${key}${included}`,
-      json: true
-    };
-  
-  return rp(matchStats)
-  .then(matchStats => {
-    return matchStats;
-  })
-  .catch(error => {
-    console.log(`matchStatsGrabber error: ${error}`);
-  });
-}
-
-exports.competitionGrabber = competitionGrabber;
-exports.seasonGrabber = seasonGrabber;
-exports.teamsGrabber = teamsGrabber;
-exports.playersGrabber = playersGrabber;
-exports.matchGrabber = matchGrabber;
-exports.matchStatsGrabber = matchStatsGrabber;
-exports.playersByTeamGrabber = playersByTeamGrabber;
+exports.leagueGrabber = leagueGrabber;
--- test/api-test.js
+++ test/api-test.js
@@ -2,16 +2,16 @@ const
     // import common modules
     { mongoose, dbTestConnection } = require('./common.js'),
     // import api functions
-    { competitionGrabber, seasonGrabber, teamsGrabber, playersGrabber, matchGrabber, matchStatsGrabber } = require('../programFunctions/api_functions.js');
+    { leagueGrabber, seasonGrabber, teamsGrabber, playersGrabber, matchGrabber, matchStatsGrabber } = require('../programFunctions/api_functions.js');
 	
-describe('Competition Grabber', () => {
-	it('should return the competition searched for', () => {
-		const competitionId = 66;
+describe('League Grabber', () => {
+	it('should return the league searched for', () => {
+		const leagueId = 501;
 		
-		return competitionGrabber(competitionId)
-		.then(competition => {
-			competition.id.should.equal(66);
-			competition.name.should.equal('Premiership');
+		return leagueGrabber(leagueId)
+		.then(league => {
+			league.id.should.equal(501);
+			league.name.should.equal('Premiership');
 		})
 		.catch(error => {
 			console.log(`error: ${error}`);
@@ -20,82 +20,82 @@ describe('Competition Grabber', () => {
 	.timeout(5000);
 });
 
-describe('Season Grabber', () => {
-	it('should return the season searched for', () => {
-		const seasonId = 741;
+// describe('Season Grabber', () => {
+// 	it('should return the season searched for', () => {
+// 		const seasonId = 741;
 		
-		return seasonGrabber(seasonId)
-		.then(season => {
-			season.id.should.equal(741);
-			season.name.should.equal('2016/2017');
-		})
-		.catch(error => {
-			console.log(`error: ${error}`);
-		});
-	}).timeout(5000);
-});
+// 		return seasonGrabber(seasonId)
+// 		.then(season => {
+// 			season.id.should.equal(741);
+// 			season.name.should.equal('2016/2017');
+// 		})
+// 		.catch(error => {
+// 			console.log(`error: ${error}`);
+// 		});
+// 	}).timeout(5000);
+// });
 
-describe('Teams Grabber', () => {
-	it('should return teams in a given season', () => {
-		const seasonId = 741;
+// describe('Teams Grabber', () => {
+// 	it('should return teams in a given season', () => {
+// 		const seasonId = 741;
 		
-		return teamsGrabber(seasonId)
-		.then(teams => {
-			teams.data[0].id.should.equal(152);
-			teams.data[0].name.should.equal('Celtic');
-		})
-		.catch(error => {
-			console.log(`error: ${error}`);
-		});
-	}).timeout(5000);
-});
+// 		return teamsGrabber(seasonId)
+// 		.then(teams => {
+// 			teams.data[0].id.should.equal(152);
+// 			teams.data[0].name.should.equal('Celtic');
+// 		})
+// 		.catch(error => {
+// 			console.log(`error: ${error}`);
+// 		});
+// 	}).timeout(5000);
+// });
 
-describe('Player Grabber', () => {
-	it('should return info for a given player', () => {
-		const playerId = 217;
+// describe('Player Grabber', () => {
+// 	it('should return info for a given player', () => {
+// 		const playerId = 217;
 		
-		return playersGrabber(playerId)
-		.then(player => {
-			player.id.should.equal(217);
-			player.name.should.equal('S. Armstrong');
-		})
-		.catch(error => {
-			console.log(`error: ${error}`);
-		});
-	}).timeout(5000);
-});
+// 		return playersGrabber(playerId)
+// 		.then(player => {
+// 			player.id.should.equal(217);
+// 			player.name.should.equal('S. Armstrong');
+// 		})
+// 		.catch(error => {
+// 			console.log(`error: ${error}`);
+// 		});
+// 	}).timeout(5000);
+// });
 
-describe('Match Grabber', () => {
-	it('should info for a given match', () => {
-		const matchId = 687992;
+// describe('Match Grabber', () => {
+// 	it('should info for a given match', () => {
+// 		const matchId = 687992;
 		
-		return matchGrabber(matchId)
-		.then(match => {
-			match.id.should.equal(687992);
-			match.home_team_id.should.equal(701);
-			match.away_team_id.should.equal(152);
-			match.homeTeam.name.should.equal('Hearts');
-			match.awayTeam.name.should.equal('Celtic');
-		})
-		.catch(error => {
-			console.log(`error: ${error}`);
-		});
-	}).timeout(5000);
-});
+// 		return matchGrabber(matchId)
+// 		.then(match => {
+// 			match.id.should.equal(687992);
+// 			match.home_team_id.should.equal(701);
+// 			match.away_team_id.should.equal(152);
+// 			match.homeTeam.name.should.equal('Hearts');
+// 			match.awayTeam.name.should.equal('Celtic');
+// 		})
+// 		.catch(error => {
+// 			console.log(`error: ${error}`);
+// 		});
+// 	}).timeout(5000);
+// });
 
-describe('Match Stats Grabber', () => {
-	it('should return stats for a given match', () => {
-		const matchId = 687993;
+// describe('Match Stats Grabber', () => {
+// 	it('should return stats for a given match', () => {
+// 		const matchId = 687993;
 		
-		return matchStatsGrabber(matchId)
-		.then(matchStats => {
-			matchStats.home.id.should.equal(28422);
-			matchStats.home.team_id.should.equal(696);
-			matchStats.away.id.should.equal(28423);
-			matchStats.away.team_id.should.equal(700);
-		})
-		.catch(error => {
-			console.log(`error: ${error}`);
-		});
-	}).timeout(5000);
-});
+// 		return matchStatsGrabber(matchId)
+// 		.then(matchStats => {
+// 			matchStats.home.id.should.equal(28422);
+// 			matchStats.home.team_id.should.equal(696);
+// 			matchStats.away.id.should.equal(28423);
+// 			matchStats.away.team_id.should.equal(700);
+// 		})
+// 		.catch(error => {
+// 			console.log(`error: ${error}`);
+// 		});
+// 	}).timeout(5000);
+// });
