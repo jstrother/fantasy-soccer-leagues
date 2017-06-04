438e33a6ab5304eb0d4a3cb11d331bf346caa217
Jim Strother <jim.strother@gmail.com>
2017-06-04 11:51:00 -0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 
-  global.DATABASE_URL || 
-  (process.env.NODE_ENV === 'production' ? 'mongodb://gameUser:gamePassword@ds161169.mlab.com:61169/fantasy-soccer-leagues' : 'mongodb://gameUser:gamePassword@ds161169.mlab.com:61169/fantasy-soccer-test');
-
-exports.PORT = process.env.PORT || 8080;
-
-exports.API_KEY = '?api_token=SeQWLg7buPRYigOcplCKBHzJQId8Gc28qsN03n7gnoUIi0AzRO7X4tA9A3xV';
