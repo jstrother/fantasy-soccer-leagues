8dfcef739f75340d2c331acd66f9b8cf488084f8
Jim Strother <strotherwebdev@gmail.com>
2017-06-08 14:48:20 +0000

    playerStatsBySeason function successful

--- programFunctions/api_functions.js
+++ programFunctions/api_functions.js
@@ -46,31 +46,58 @@ function seasonByLeague(leagueId) {
 // this function is to retrieve all players in particular season
 function playersBySeason(seasonId) {
   const endpoint = `${baseURL}/teams/season/`,
-      included = `${toInclude}squad`,
-      teams = {
-        uri: `${endpoint}${seasonId}${key}${included}`,
-        json: true
-      };
-      
-    return rp(teams)
-    .then(teams => {
-      let playerList = [];
-      for (let i = 0; i < teams.data.length; i++) {
-        for (let j = 0; j < teams.data[i].squad.data.length; j++) {
-          playerList.push(teams.data[i].squad.data[j].player_id);
-        }
+    included = `${toInclude}squad`,
+    teams = {
+      uri: `${endpoint}${seasonId}${key}${included}`,
+      json: true
+    };
+    
+  return rp(teams)
+  .then(teams => {
+    let playerList = [];
+    for (let i = 0; i < teams.data.length; i++) {
+      for (let j = 0; j < teams.data[i].squad.data.length; j++) {
+        playerList.push(teams.data[i].squad.data[j].player_id);
       }
-      console.log('playerList:', playerList);
-      return playerList;
-    })
-    .catch(error => {
-      console.log(`playersBySeason error: ${error}`);
-    });
+    }
+    // console.log('playerList:', playerList);
+    return playerList;
+  })
+  .catch(error => {
+    console.log(`playersBySeason error: ${error}`);
+  });
 }
 
 // playersBySeason(914);
 
+// this function is to retrieve player stats
+function playerStatsBySeason(playerId, seasonId) {
+  const endpoint = `${baseURL}/players/`,
+    included = `${toInclude}stats,team`,
+    player = {
+      uri: `${endpoint}${playerId}${key}${included}`,
+      json: true
+    };
+  
+  rp(player)
+  .then(player => {
+    let playerSeasonStats =[];
+    for (let i = 0; i < player.data.stats.data.length; i++) {
+      if (player.data.stats.data[i].season_id === seasonId) {
+        playerSeasonStats.push(player.data.stats.data[i]);
+      }
+    }
+    console.log('player:', playerSeasonStats);
+    return playerSeasonStats;
+  })
+  .catch(error => {
+    console.log(`playerStats error: ${error}`);
+  });
+}
+
+// playerStatsBySeason(918, 914);
 
 exports.allLeagues = allLeagues;
 exports.seasonByLeague = seasonByLeague;
 exports.playersBySeason = playersBySeason;
+exports.playerStatsBySeason = playerStatsBySeason;
