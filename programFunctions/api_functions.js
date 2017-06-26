00cde2dea6025974ae4eb4ea7f7d4ed8633533b3
Jim Strother <strotherwebdev@gmail.com>
2017-06-26 18:33:42 +0000

    forgot to save last night

--- programFunctions/api_functions.js
+++ programFunctions/api_functions.js
@@ -142,29 +142,35 @@ function playerStatsByLeague(leagueId) {
         playerIdList = [... new Set(playerIdList)];
         
         playerIdList.forEach(playerId => {
-          // const endpoint2 = `${baseURL}/players/`,
-          //   included2 = `${toInclude}team,position`,
-          //   results2 = {
-          //     uri: `${endpoint2}${playerId}${key}${included2}`,
-          //     json: true
-          //   };
+          const endpoint2 = `${baseURL}/players/`,
+            included2 = `${toInclude}team,position`,
+            results2 = {
+              uri: `${endpoint2}${playerId}${key}${included2}`,
+              json: true
+            };
           
-          // return rp(results2)
-          // .then(results2 => {
-          //   let playerInfo = {
-          //     id: results2.data.player_id,
-          //     commonName: results2.data.player_name,
-          //     fullName: results2.data.fullname,
-          //     firstName: results2.data.firstname,
-          //     lastName: results2.data.lastname,
-          //     playerPicture: results2.data.image_path,
-              
-          //   };
-          // })
-          // .catch(error => {
-          //   console.log(`playerIdList search error: ${error}`);
-          // });
+          return rp(results2)
+          .then(results2 => {
+            let playerInfo = {
+              id: results2.data.player_id,
+              commonName: results2.data.player_name,
+              fullName: results2.data.fullname,
+              firstName: results2.data.firstname,
+              lastName: results2.data.lastname,
+              playerPosition: results2.data.position.data.name,
+              playerPicture: results2.data.image_path,
+              playerClubId: results2.data.team.data.id,
+              playerClubName: results2.data.team.data.name,
+              playerClubLogo: results2.data.team.data.logo_path
+            };
+            allData.playerMasterList.push(playerInfo);
+          })
+          .catch(error => {
+            console.log(`playerIdList search error: ${error}`);
+          });
         });
+        
+        console.log(allData.playerMasterList);
         
         stage.rounds.data.forEach(round => {
           let roundInfo = {};
@@ -500,31 +506,7 @@ function playerStatsByLeague(leagueId) {
   });
 }
 
-playerStatsByLeague(779);
+// playerStatsByLeague(779);
 
 exports.leagueSelector = leagueSelector;
 exports.playerStatsByLeague = playerStatsByLeague;
-
-const endpoint2 = `${baseURL}/players/`,
-  included2 = `${toInclude}team,position`,
-  results2 = {
-    uri: `${endpoint2}918${key}${included2}`,
-    json: true
-  };
-
-return rp(results2)
-.then(results2 => {
-  console.log(results2.data.team);
-  let playerInfo = {
-    id: results2.data.player_id,
-    commonName: results2.data.player_name,
-    fullName: results2.data.fullname,
-    firstName: results2.data.firstname,
-    lastName: results2.data.lastname,
-    playerPicture: results2.data.image_path,
-    
-  };
-})
-.catch(error => {
-  console.log(`playerIdList2 search error: ${error}`);
-});
