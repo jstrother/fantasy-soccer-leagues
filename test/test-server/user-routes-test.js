d95dd51e8e1bc47f53ef56497f3c342e3b12029f
Jim Strother <strotherwebdev@gmail.com>
2017-11-05 02:23:59 +0000

    reset user-routes-test.test.js

--- test/test-server/user-routes-test.js
+++ test/test-server/user-routes-test.js
@@ -1,9 +1,7 @@
 const express = require('express'),
+	nock = require('nock'),
 	config = require('../../server/config.js'),
-	passport = require('passport'),
-	gStrategy = require('passport-google-oauth20').Strategy,
-	bStrategy = require('passport-http-bearer').Strategy,
-  userRouter = express.Router(),
+	userRoutes = require('../../server/user-routes.js'),
 	{ chai, chaiHTTP, should, expect, mongoose, dbTestConnection } = require('../common.js'),
   User = require('../../models/user_model.js'),
   { createData, readData, updateData, deleteData } = require('../../server/programFunctions/crud_functions.js'),
@@ -21,25 +19,35 @@ before(() => {
 });
 
 describe('Selects League', () => {
-	it('fantasyLeagueId and fantasyLeagueName should not exist in a user profile', () => {
-		
-		return createData(testCurrentUser, User)
-		.then(() => {
-			chai.request(app)
-			.get('/user/user1')
-			.set({'Authorization': `Bearer ${accessToken}`})
-			.then(res => {
-				console.log('res.body:', res.body);
-				console.log('res.body keys:', Object.keys(res.body));
-				res.body.should.not.be.empty;
-				res.body.should.have.property('fantasyLeagueId', fantasyLeagueId);
-				res.body.should.have.property('fantasyLeagueName', fantasyLeagueName);
-			})
-			.catch(err => {
-				throw new Error(err);
-			});
-		});
+	it('user profile should not exist', () => {
+		const sampleUser = {
+ 			displayName: 'user1',
+ 			accessToken,
+ 			googleId: '1974',
+ 			givenName: 'Test',
+ 			familyName: 'User'
+ 		};
+ 		return createData(sampleUser, User)
+ 		.then(() => {
+ 			console.log('hello');
+ 			chai.request(app)
+ 			.get('/user/user1')
+ 			.set({'Authorization': `Bearer ${accessToken}`})
+ 			.then(res => {
+ 				console.log('res.body:', res.body);
+ 				console.log('res.body keys:', Object.keys(res.body));
+ 				expect(res.body).to.not.be.empty;
+ 				expect(res.body).to.have.property('fantasyLeagueId', fantasyLeagueId);
+ 				expect(res.body).to.have.property('fantasyLeagueName', fantasyLeagueName);
+ 			})
+ 			.catch(err => {
+ 				throw new Error(err);
+ 			});
+ 		});
 	});
+	// res.body.should.not.be.empty;
+	// res.body.should.have.property('fantasyLeagueId', fantasyLeagueId);
+	// res.body.should.have.property('fantasyLeagueName', fantasyLeagueName);
 	
 	it('should add league id and name to a user profile', () => {
 		// chai.request(app)
