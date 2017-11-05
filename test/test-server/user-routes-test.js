bd07a98480d9ebc3ce0f9d7469b5955bc5a5864d
Jim Strother <strotherwebdev@gmail.com>
2017-11-05 03:53:25 +0000

    user-routes-test.test.js has working tests, but they seem too simple

--- test/test-server/user-routes-test.js
+++ test/test-server/user-routes-test.js
@@ -1,60 +1,93 @@
 const express = require('express'),
 	nock = require('nock'),
+	chai = require('chai'),
+	chaiHTTP = require('chai-http'),
+	should = chai.should(),
+	expect = chai.expect,
 	config = require('../../server/config.js'),
 	userRoutes = require('../../server/user-routes.js'),
-	{ chai, chaiHTTP, should, expect, mongoose, dbTestConnection } = require('../common.js'),
+	{ mongoose, dbTestConnection } = require('../common.js'),
   User = require('../../models/user_model.js'),
   { createData, readData, updateData, deleteData } = require('../../server/programFunctions/crud_functions.js'),
-  fantasyLeagueId = 779,
-  fantasyLeagueName = 'Major League Soccer (USA)',
   { runServer, app } = require('../../server/server.js'),
-  testCurrentUser = require('../currentUser.js'),
-  accessToken = 42;
+  testCurrentUser = {
+  	accessToken: 1974,
+	  displayName: 'Clint Dempsey',
+	  givenName: 'Clint',
+	  familyName: 'Dempsey',
+	  userPhoto: 'http://ww2.hdnux.com/photos/61/57/52/13040273/3/rawImage.jpg',
+	  googleId: 2
+  },
+  fantasyLeagueId = 779,
+  fantasyLeagueName = 'Major League Soccer (USA)';
 
 chai.use(chaiHTTP);
 mongoose.Promise = Promise;
     
 before(() => {
-	return runServer(8081, dbTestConnection);
+	runServer(8081, dbTestConnection);
 });
 
-describe('Selects League', () => {
-	it('user profile should not exist', () => {
-		const sampleUser = {
- 			displayName: 'user1',
- 			accessToken,
- 			googleId: '1974',
- 			givenName: 'Test',
- 			familyName: 'User'
- 		};
- 		return createData(sampleUser, User)
- 		.then(() => {
- 			console.log('hello');
- 			chai.request(app)
- 			.get('/user/user1')
- 			.set({'Authorization': `Bearer ${accessToken}`})
- 			.then(res => {
- 				console.log('res.body:', res.body);
- 				console.log('res.body keys:', Object.keys(res.body));
- 				expect(res.body).to.not.be.empty;
- 				expect(res.body).to.have.property('fantasyLeagueId', fantasyLeagueId);
- 				expect(res.body).to.have.property('fantasyLeagueName', fantasyLeagueName);
- 			})
- 			.catch(err => {
- 				throw new Error(err);
- 			});
- 		});
+describe('User Profile', () => {
+	it('should not exist', () => {
+		const nonExistantCurrentUser = testCurrentUser;
+		
+		return readData(nonExistantCurrentUser, User).should.eventually.not.exist;
 	});
 	
-	it('should add league id and name to a user profile', () => {
-		// chai.request(app)
-		// 	.put('/user/addLeague')
-		// 	.send({ fantasyLeagueId, fantasyLeagueName })
-		// 	.end((err, res) => {
-		// 		if (err) {
-		// 			throw new Error(err);
-		// 		}
-				
-		// 	});
+	it('should create a user profile', () => {
+		const createCurrentUser = testCurrentUser;
+		
+		return createData(createCurrentUser, User).should.eventually.exist;
+	});
+	
+	it('should update a user profile', () => {
+		const updateCurrentUser = testCurrentUser;
+		
+		return updateData(updateCurrentUser, { fantasyLeagueId, fantasyLeagueName }, User)
+		.then(updatedItem => {
+			updatedItem.should.have.property('fantasyLeagueId', fantasyLeagueId);
+			updatedItem.should.have.property('fantasyLeagueName', fantasyLeagueName);
+		});
+	});
+	
+	it('should remove a user profile', () => {
+		const deleteCurrentUser = {
+			accessToken: testCurrentUser.accessToken,
+			googleId: testCurrentUser.googleId,
+			displayName: testCurrentUser.displayName,
+			givenName: testCurrentUser.givenName,
+			familyName: testCurrentUser.familyName,
+			userPhoto: testCurrentUser.userPhoto,
+			fantasyLeagueId,
+			fantasyLeagueName
+		};
+		
+		return deleteData(deleteCurrentUser, User)
+		.then(deletedItem => {
+			readData(deleteCurrentUser, User)
+			.then(deletedItem => {
+				deletedItem.should.not.exist;
+			});
+		});
 	});
 });
+
+// .then(() => {
+			// 	console.log('hello');
+			// 	chai.request(app)
+			// 	.get('/user/user1')
+			// 	.set({'Authorization': `Bearer ${accessToken}`})
+			// 	.then(res => {
+			// 		// console.log('res:', res);
+			// 		console.log('res.body:', res.body);
+			// 		console.log('res.body keys:', Object.keys(res.body));
+			// 		true.should.equal(false);
+			// 		res.should.be.empty;
+			// 		res.should.have.property('fantasyLeagueId', fantasyLeagueId);
+			// 		res.should.have.property('fantasyLeagueName', fantasyLeagueName);
+			// 	})
+			// 	.catch(err => {
+			// 		throw new Error(err);
+			// 	});
+			// });
