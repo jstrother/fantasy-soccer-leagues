62096141eea80f9aa31ea31301e9f9fa247682d0
Jim Strother <strotherwebdev@gmail.com>
2018-03-15 04:49:53 +0000

    standingsCalculator() working

--- server/programFunctions/scheduleCreation_function.js
+++ server/programFunctions/scheduleCreation_function.js
@@ -4,7 +4,24 @@ const mongoose = require('mongoose'),
   FantasyClub = require("../../models/fantasyClub_model.js"),
   WeeklyMatches = require("../../models/weeklyMatches.js");
 
-// matchArray will be filled by getting schedule.matches from fantasySchedule-routes.js
+function standingsCalculator(clubArray) {
+  clubArray.sort((a, b) => compare(a.points, b.points) || compare(a.goalDifferential, b.goalDifferential) || compare(a.goalsFor, b.goalsFor));
+  return clubArray;
+  
+  function compare(a, b) {
+    if(a < b) {
+      return 1;
+    }
+    if(a > b) {
+      return -1;
+    }
+    if( a === b) {
+      return 0;
+    }
+  }
+}
+
+// matchArray will be filled by getting schedule.matches.weeklyMatches from fantasySchedule-routes.js
 // can use loopArray_function.js to run matchResolver() once a week on the correct index in matchArray
 function matchResolver(matchArray) {
   let allScores = 0,
@@ -170,6 +187,7 @@ module.exports = {
   scheduleCreator,
   matchCreator,
   matchResolver,
+  standingsCalculator,
   save,
   scheduleCreatorPromise
 };
--- test/test-server/scheduleCreation-test.js
+++ test/test-server/scheduleCreation-test.js
@@ -1,6 +1,7 @@
 const { mongoose, chai, chaiHTTP, firstClub, secondClub, thirdClub, fourthClub, fifthClub, sixthClub } = require('../common.js'),
-  { save, matchCreator, scheduleCreator, matchResolver } = require("../../server/programFunctions/scheduleCreation_function.js"),
-  FantasyMatch = require("../../models/fantasyMatch_model.js");
+  { save, matchCreator, scheduleCreator, matchResolver, standingsCalculator } = require("../../server/programFunctions/scheduleCreation_function.js"),
+  FantasyMatch = require("../../models/fantasyMatch_model.js"),
+  FantasyClub = require("../../models/fantasyClub_model.js");
 
 chai.use(chaiHTTP);
 mongoose.Promise = Promise;
@@ -11,13 +12,13 @@ describe('Fantasy Schedule',() => {
     save(secondClub);
     
     return matchCreator(firstClub, secondClub).should.exist;
-  });
+  }).timeout(5000);
   
   it('should create a league schedule', () => {
     let clubArray = [firstClub, secondClub, thirdClub, fourthClub, fifthClub];
     
     return scheduleCreator(clubArray).should.eventually.exist;
-  });
+  }).timeout(5000);
   
   it('should resolve match outcomes', () => {
     // these test are not set up to clear from the database after each and every test, so we just tack on the rest of the clubs here and build up from the mathCreator() test above
@@ -38,9 +39,13 @@ describe('Fantasy Schedule',() => {
       .then(data => {
         matchResolver(data).should.exist;
       });
-  });
+  }).timeout(5000);
   
-  // it('should calculate the league standings', () => {
-    
-  // });
+  it('should calculate the league standings', () => {
+    return FantasyClub
+      .find()
+      .then(data => {
+        standingsCalculator(data);
+      });
+  }).timeout(5000);
 });
