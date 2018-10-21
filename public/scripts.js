e2cc8dfa769d12ecea8f0227674f1510d877ba07
Jim Strother <jim.strother@gmail.com>
2018-10-21 15:13:25 -0500

    standings page looking good on moblie

--- public/scripts.js
+++ public/scripts.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "7526488b6695419fd60a"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "b201b16493a32b4334d7"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
