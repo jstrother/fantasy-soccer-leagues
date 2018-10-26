507817e921bc699dc66684c35e7a7a382393ea5e
Jim Strother <jim.strother@gmail.com>
2018-10-25 19:34:02 -0500

    app working correctly on localhost

--- public/scripts.js
+++ public/scripts.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "9fece8f0e8462413479a"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "b6e4f550eb7363170116"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -5209,9 +5209,7 @@ exports.CLIENT_SECRET = 'yIs-UYd9Rqqe8GVS1DRB5Aij';
 exports.CLIENT_ID = '37522725082-dlubl11l5pbgcibrtq5r40og5m1af9jd.apps.googleusercontent.com';
 exports.LEAGUE_LOOP_REPEAT_TIME = 30 * 60 * 1000; // # of minutes league loop should repeat; also helps limit continuous looping of playerIdArrays to once per league loop
 
-var DEV_DIRECTORY = 'http://127.0.0.1:8080';
-exports.DIRECTORY = DEV_DIRECTORY; // const PROD_DIRECTORY = 'https://the-fsfsl.herokuapp.com';
-// exports.DIRECTORY = PROD_DIRECTORY;
+exports.DIRECTORY = 'http://127.0.0.1:8080'; // exports.DIRECTORY = 'https://the-fsfsl.herokuapp.com';
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
